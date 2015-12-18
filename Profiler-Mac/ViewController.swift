dfc17f685fdac6f63a6f0766f27fd66cc7d91828
Janek Spaderna <janek@spaderna.eu>
2015-12-18 23:16:13 +0100

    Add a Mac Profiler target

---
+++ Profiler-Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Profiler-Mac
+//
+//  Created by Janek Spaderna on 18.12.15.
+//  Copyright © 2015 jds. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
